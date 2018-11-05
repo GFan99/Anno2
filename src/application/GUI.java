fa035363102253eb8c9990fcca13e7b96a927466
GFan99 <GFan99@users.noreply.github.com>
2018-11-05 13:56:23 +0100

    Beschreibung GUI

--- src/application/GUI.java
+++ src/application/GUI.java
@@ -16,7 +16,27 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
-
+/**
+ * Dies wird die Klasse GUI.
+ * In ihr wird die gesamte graphische Oberflaeche progammiert.
+ *  - start()		 : Diese Methode startet die GUI.
+ *  - idabfrage()	 : Diese Funktion soll den Nutzer nach seiner ID fragen und diese dann auch zurück geben.
+ *  - getLabel()  	 : Diese Methode soll die vom Nutzer ausgewaehlten Label erfassen und speichern. Dazu
+ *  					nutzt sie zunaechst die Funktion Klassifikator.waehleText() und stellt den zur
+ *  					erhaltenen TextID gehoerigen Text dar. Dann wartet sie auf ein Clicked-Event des 
+ *  					OK-Buttons und prueft dann die Eingaben auf Vollstaendigkeit, bevor sie die zu 
+ *  					den Labels erfassten Werte in einem Array speichert, welches sie zusammen mit der
+ *  					textID der Methode Klassifikator.schreibeText() uebergibt.
+ *  					Sie beginnt nun ihre Arbeit von vorne, solange bis sie durch 
+ *  					Klassifikator.waehleText() den Abbruch-Code uebergeben bekommt. Sie schliesst dann
+ *  					die GUI.
+ *  - schliessen()	 : Beendet die graphische Darstellung und startet den Speichervorgang.
+ *  - abbruch()		 : Diese Methode wird ausgeloest, wenn die graphische Oberflaeche vom Nutzer vorzeitig
+ *  					geschlossen wird. Sie startet die "Abbruchspeicherung".
+ *  - fortschritt	 : Zeigt ein kleines Fenster mit dem Fortschritt.
+ * @author becksusanna
+ * @version 0.1
+ */
 public class GUI extends Application {
 	@Override
 	public void start(Stage primaryStage) {
