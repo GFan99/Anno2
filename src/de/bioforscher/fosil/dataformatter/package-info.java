287a622255a64288b3cc7c7c3ba3b275ac7522fa
paddelbaer <40768309+paddelbaer@users.noreply.github.com>
2018-12-07 11:35:12 +0100

    DataFormatter bearbeitet + neue Klassen erstellt

--- src/de/bioforscher/fosil/dataformatter/package-info.java
+++ src/de/bioforscher/fosil/dataformatter/package-info.java
@@ -2,7 +2,7 @@
 // Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.2.8-b130911.1802 generiert 
 // Siehe <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
-// Generiert: 2018.11.19 um 01:22:49 PM CET 
+// Generiert: 2018.12.07 um 08:46:12 AM CET 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://www.bioforscher.de/fosil/DataFormatter", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
