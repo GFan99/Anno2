f559b358e313a0a7304893d01962edd1a4106430
GFan99 <GFan99@users.noreply.github.com>
2018-12-09 20:49:09 +0100

    Update Timer.java
    kann quasi gelöscht werden...

--- src/application/Timer.java
+++ src/application/Timer.java
@@ -8,19 +8,23 @@ import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.stage.WindowEvent;
 import javafx.util.Duration;
 
-public class Timer {
+public class Timer extends Pane{
 	private static final Integer timeInMin = 60;
     private Timeline timeline;
     private Label timerLabel = new Label();
     private Integer timeInSec = timeInMin*60;
     private Button button = new Button();
     
-    public void timer(int zeitInMin) {
+    public Timer(int zeitInMin) {
+    	super();
     	timerLabel.setText(timeInMin.toString()+":00");
-    	EventHandler<MouseEvent> timerdown=new EventHandler<MouseEvent>() {
-    		public void handle(MouseEvent e) {
+    	this.getChildren().addAll(timerLabel);
+    	EventHandler<WindowEvent> timerdown=new EventHandler<WindowEvent>() {
+    		public void handle(WindowEvent e) {
     			if (timeline != null) {
     	            timeline.stop();
     	        }
@@ -46,8 +50,49 @@ public class Timer {
     	        timeline.playFromStart();
     		}
     	};
-    	button.addEventFilter(MouseEvent.MOUSE_CLICKED, timerdown);
+    	timerLabel.addEventFilter(WindowEvent.ANY,timerdown);
     	
     }
     
 }
+/**
+public class Timer {
+private static final Integer timeInMin = 60;
+private Timeline timeline;
+private Label timerLabel = new Label();
+private Integer timeInSec = timeInMin*60;
+Button button = new Button();
+
+public void timer(int zeitInMin) {
+	timerLabel.setText(timeInMin.toString()+":00");
+	EventHandler<MouseEvent> timerdown=new EventHandler<MouseEvent>() {
+		public void handle(MouseEvent e) {
+			if (timeline != null) {
+	            timeline.stop();
+	        }
+	        timeInSec = timeInMin*60;
+	 
+	        // update timerLabel
+	        int min = timeInSec/60;
+            int sec = timeInSec%60;
+	        timerLabel.setText(min+":"+sec);
+	        timeline = new Timeline();
+	        timeline.setCycleCount(Timeline.INDEFINITE);
+	        timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(1),new EventHandler<ActionEvent>() {
+	                    public void handle(ActionEvent e) {
+	                        timeInSec--;
+	                        int min=timeInSec/60;
+	                        int sec = timeInSec%60;
+	                        timerLabel.setText(min+":"+sec);
+	                        if (timeInSec <= 0) {
+	                            timeline.stop();
+	                        }
+	                      }
+	                }));
+	        timeline.playFromStart();
+		}
+	};
+	button.addEventFilter(MouseEvent.MOUSE_CLICKED, timerdown);
+	
+}
+*/
