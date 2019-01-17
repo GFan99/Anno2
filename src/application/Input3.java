28391d51aba35761702a9388f8f903cf6bd5c3a3
paddelbaer <40768309+paddelbaer@users.noreply.github.com>
2019-01-17 15:20:41 +0100

    Update Input3.java

--- src/application/Input3.java
+++ src/application/Input3.java
@@ -35,7 +35,7 @@ public class Input3 {
 	public static File pfadNachOS(String dateiname, String ordnername) {
 		String osName = System.getProperty("os.name");
 		if (osName.indexOf("Windows") != -1) {
-			String pfad = System.getProperty("user.dir");//user.dir ist workspace
+			String pfad = System.getProperty("user.home");//user.dir ist workspace
 			if (dateiname!="") {
 				dateiname="//"+dateiname;
 			}
